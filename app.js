b5406038c440d1475b610f86d1c8de793ef22f7e
Edu <edugacv@gmail.com>
2020-12-23 13:45:58 +0100

    All bonus done

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // tu código aquí
 const globos = document.querySelectorAll('.balloon')
-let contador = 0
+let contador = 24
+let printContador = document.querySelector('#counter')
+let reset = document.querySelector('.btn-reset')
 
+printContador.innerHTML = contador
 globos.forEach(explodeBallon)
 
 function explodeBallon(globo){
@@ -12,15 +15,49 @@ function petarGlobo(e){
     if(e.target.className === 'balloon'){
         e.target.textContent = 'POP!'
         e.target.style.backgroundColor = '#ededed'
-        contador++
+        contador--
+        printContador.innerHTML = contador
         e.target.removeEventListener('mouseover', petarGlobo)
         allExplode()
     }
 }
 
 function allExplode(){
-    if(contador === 24){
+    if(contador === 0){
         document.querySelector('#yay-no-balloons').style.display = 'inline'
         document.querySelector('#balloon-gallery').style.display = 'none'
+        printContador.innerHTML = 'GG!'
     }
+}
+
+//Reset Balloons
+globos.forEach(inflateBallons)
+
+function inflateBallons(globo){
+    reset.addEventListener('click', function(){
+        globo.textContent = ''
+        globo.style.backgroundColor = ''
+        contador = 24
+        printContador.innerHTML = 24
+        globo.addEventListener('mouseover', petarGlobo)
+        if(contador === 24){
+            document.querySelector('#yay-no-balloons').style.display = 'none'
+            document.querySelector('#balloon-gallery').style.display = 'inline'
+            contador = 24
+            printContador.innerHTML = 24
+        }
+    })
+}
+
+//Inflate Single Ballon
+globos.forEach(singleBallon)
+
+function singleBallon(globo){
+        globo.addEventListener('dblclick', function(){
+        globo.textContent = ''
+        globo.style.backgroundColor = ''
+        contador = contador + 1
+        printContador.innerHTML = contador
+        globo.addEventListener('mouseover', petarGlobo)
+    })
 }
