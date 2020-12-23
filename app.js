e75119cb35b7d9b44e6e60b76c56a789089e924e
Edu <edugacv@gmail.com>
2020-12-23 12:34:19 +0100

    Exercice finished

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 // tu código aquí
+const globos = document.querySelectorAll('.balloon')
+let contador = 0
+
+globos.forEach(explodeBallon)
+
+function explodeBallon(globo){
+    globo.addEventListener('mouseover', petarGlobo)    
+}
+
+function petarGlobo(e){
+    if(e.target.className === 'balloon'){
+        e.target.textContent = 'POP!'
+        e.target.style.backgroundColor = '#ededed'
+        contador++
+        e.target.removeEventListener('mouseover', petarGlobo)
+        allExplode()
+    }
+}
+
+function allExplode(){
+    if(contador === 24){
+        document.querySelector('#yay-no-balloons').style.display = 'inline'
+        document.querySelector('#balloon-gallery').style.display = 'none'
+    }
+}
